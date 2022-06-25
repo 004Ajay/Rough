6baf2417aed5c89a18d361d0eaf1eb451adb9a38
Ajay T Shaju <ajaytshaju@gmail.com>
2022-06-25 17:06:14 +0530

    incomplete

--- For Graph Plotting/csvFileGraph.py
+++ For Graph Plotting/csvFileGraph.py
@@ -1,22 +1,35 @@
-#---------------------
-#incomplete
-#---------------------
+
+"""
+----------------------
+INCOMPLETE
+----------------------
+INCOMPLETE
+----------------------
+"""
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
-csvFile = pd.read_csv('ADL_202_ex.csv')
+df = pd.read_csv('ADL_202_ex.csv') # Opening CSV File 
 
-col1 = csvFile['Mark'].tolist()
-col2 = csvFile['Gender'].tolist()
+# Table of Student's information
+print("Student's Information\nTotal\tMales\tFemales") # Headings
+print("{}\t{}\t{}".format(df['Gender'].count(), 
+                          df['Gender'].value_counts().m,
+                          df['Gender'].value_counts().f)) #format() used
 
-plt.grid()
-plt.title("Students Mark Graph")
-plt.scatter(col1, col2)
-plt.xlabel('Mark')
-plt.ylabel('Gender')
-plt.show()
+col1 = df['Mark'] # Gettting & storing MARKS to col1 
+col2 = df['Gender'] # Gettting & storing GENDER to col2
 
-#---------------------
-#incomplete
-#---------------------
+# Table of Mean, Max, Min & Class Average
+print("\n\nClass Average:", int(col1.mean()),
+      "\nMedian:", col1.median(),
+      "\nMode:", col1.mode()) # showing mean, median & mode
+
+"""
+----------------------
+INCOMPLETE
+----------------------
+INCOMPLETE
+----------------------
+"""
