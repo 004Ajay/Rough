012c65c9e2df7f80eeba926c2edb7e03dde0dde9
Ajay T Shaju <ajaytshaju@gmail.com>
2022-06-25 11:17:04 +0530

    final program

--- For Graph Plotting/RectifierGraph.py
+++ For Graph Plotting/RectifierGraph.py
@@ -1,29 +1,13 @@
-#---------------------
-#incomplete
-#---------------------
+# Question: write a program to plot a graph for Full wave rectifier output
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 100 linearly spaced numbers
-x = np.linspace(-np.pi,np.pi,100)
+x = np.linspace(0, 2*np.pi, 10000)
+y = np.sin(x)
+z = abs(y)
 
-p = np.sin(x) # y = sin(x)
-q = np.sin(-x) # y = sin(2x)
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.spines['left'].set_position('center')
-ax.spines['bottom'].set_position('center')
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-
-plt.plot(x,p, 'b-', label='y=sin(x)')
-plt.plot(x,q, 'c-', label='y=sin(2x)')
+plt.title("Rectifier Graph")
+plt.plot(x,y)
+plt.plot(x, z)
 plt.show()
-
-#---------------------
-#incomplete
-#---------------------
