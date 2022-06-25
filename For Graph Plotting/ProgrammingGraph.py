906b2bfd6986e56aaf96e0358f73aca4fff8f6b6
Ajay T Shaju <ajaytshaju@gmail.com>
2022-06-25 11:05:01 +0530

    variable name change and grid removed

--- For Graph Plotting/ProgrammingGraph.py
+++ For Graph Plotting/ProgrammingGraph.py
@@ -1,9 +1,8 @@
 import  matplotlib.pyplot as plt
-lang = ['C', 'C++', 'Java', 'Python', 'PHP']
-students = [28, 34, 26, 40, 21]
+language = ['C', 'C++', 'Java', 'Python']
+students = [28, 34, 26, 40]
 plt.title("Programming")
 plt.xlabel('X : axis')
 plt.ylabel('Y : axis')
-plt.bar(lang, students)
-plt.grid()
+plt.bar(language, students)
 plt.show()
