58870b037dc4b70876a152b538bfdf2ed94d75b4
Ajay T Shaju <ajaytshaju@gmail.com>
2022-06-27 08:02:04 +0530

    ImgRecog

---
+++ For ML/ImageRecog.py
@@ -0,0 +1,14 @@
+#--------------
+# Doesn't Work
+#--------------
+
+from imageai.Classification.Custom import ClassificationModelTrainer
+
+model_trainer = ClassificationModelTrainer()
+model_trainer.setModelTypeAsResNet50()
+model_trainer.setDataDirectory("idenprof")
+model_trainer.trainModel(num_objects=10, num_experiments=200, enhance_data=True, batch_size=32, show_network_summary=True)
+
+#--------------
+# Doesn't Work
+#--------------
