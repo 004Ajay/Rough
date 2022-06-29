e4b14cce791de48a67f4fa653618ba45168c694a
Ajay T Shaju <ajaytshaju@gmail.com>
2022-06-30 00:29:52 +0530

    minor change

--- For Guess/guesser.py
+++ For Guess/guesser.py
@@ -2,13 +2,16 @@ import random
 
 def guess(limit, SecretNum):
     guess = random.randint(1, limit)
+    steps = 0
     while guess != SecretNum:
+        steps += 1
         guess = random.randint(1, limit)
         if guess < SecretNum:
             print(f"{guess} is too low")
         elif guess > SecretNum:
             print(f"{guess} is too high")
-    print(f'Computer correctly guessed, {SecretNum}')
+    print(f'Computer correctly guessed, {SecretNum}\nNumber of steps: {steps-1}') # steps - 1 for omitting the false run of loop
+
 
 while True:
     LimitNum = int(input("Enter a limit number: "))
