4fdf252ba77d92812fd31533458d7f181a10aa5a
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-06 00:41:32 +0530

    code correction

--- For OS/Semaphore.c
+++ For OS/Semaphore.c
@@ -1,50 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
-int mutex = 1, full = 0, empty = 3, x=0, n;
+int mutex = 1, full = 0, empty = 3, x=0;
 
 int wait(int s){
-return(s--);
+return(--s);
 }
 int signal(int s){
-return(s++);
+return(++s);
 }
 void producer(){
     mutex = wait(mutex);
     full = signal(full);
     empty = wait(empty);
     x++;
-    printf("\nproducer produces the item%d",x);
+    printf("\nproducer produces the item %d", x);
     mutex = signal(mutex);
     }
 void consumer(){
     mutex = wait(mutex);
     full = wait(full);
     empty = signal(empty);
-    printf("\n consumer consumes item%d",x);
+    printf("\nconsumer consumes item %d", x);
     x--;
     mutex = signal(mutex);
     }
 
-int main(){
-printf("\n1.PRODUCER\n2.CONSUMER\n3.EXIT\n");
+main(){
+int n;
+printf("\n1: Producer\n2: Consumer\n3: Exit\n");
 while(1){
-printf("\nChoice: \n");
-scanf("%d",&n);
+printf("\nChoice: ");
+scanf("%d", &n);
 switch(n){ 
-case 1: if((mutex == 1)&&(empty != 0))
-        producer();
-        else
-        printf("Buffer is Full");
-        break;
-
-case 2: if((mutex == 1)&&(full != 0))
-        consumer();
-        else
-        printf("Buffer is Empty");
-        break;
+    case 1: if((mutex == 1) && (empty != 0))
+            producer();
+            else
+            printf("Buffer is Full");
+            break;
+    case 2: if((mutex == 1) && (full != 0))
+            consumer();
+            else
+            printf("Buffer is Empty");
+            break;
 case 3: exit(0);
-default: printf("Choice exceeds limit.");
+        break;
         }
     }
-return 0;
+//return 0;
 }
