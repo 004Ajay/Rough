4c66940ffc6961eeef41a9b248dfd01e1a412f3a
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-06 23:52:39 +0530

    next change

--- test2.py
+++ test2.py
@@ -11,7 +11,7 @@ print(f"Correlation value:\n{corrValue}") # For simplicity, f-string is used
 # To find percentage of correlation using pearsonr
 lst = list(pearsonr(hand, height))
 print("Pearsonr value: ", lst)
-if lst[1] > 0.05:
+if lst[1] > 0.05: # Not sure about this...
     print(f"We reject Null Hypothesis, since, {round(lst[1], 5)} is greater than 0.05")
 else:
     print("We accept Null Hypothesis")
