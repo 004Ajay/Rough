f297da383f1858183f5edeb1a53befe8cd7cec58
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-11 20:35:06 +0530

    temp files

--- For Graph/sample.py
+++ For Graph/sample.py
@@ -2,4 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_excel("Mohanlal.xlsx")
-print(df)
+
+year = df['Year'] < 2000
+m_name = df.loc[df['Movie Name']]
+
+print(year)
+#print(m_name)
+#plt.scatter()
