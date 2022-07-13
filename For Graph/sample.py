44ae7c8845c0564740895cd5eb6611605758a41e
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-13 16:55:41 +0530

    test

--- For Graph/sample.py
+++ For Graph/sample.py
@@ -3,9 +3,13 @@ import pandas as pd
 
 df = pd.read_excel("Mohanlal.xlsx")
 
+print(df.head())
+
+"""
 year = df['Year'] < 2000
 m_name = df.loc[df['Movie Name']]
 
 print(year)
 #print(m_name)
 #plt.scatter()
+"""
