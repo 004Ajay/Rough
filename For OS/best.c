76240f82c062c3d0f12528ba891e94d978dcf6a0
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-14 18:27:04 +0530

    edits

--- For OS/best.c
+++ For OS/best.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
+
 int main(){
-int b[20], f[20], bno, fno, i, j, temp, barr[20], farr[20];
+int blk[20], fl[20], block_no, fl_no, i, j, temp, barr[20], blk_no[20], lowest = 9999;
 printf("\nMemory Management Scheme - Best Fit");
 printf("\nEnter number of blocks: ");
-scanf("%d", &bno);
-int lowest = 9999;
+scanf("%d", &block_no);
+
 printf("Enter size of blocks\n");
-for (i = 1; i <= bno; i++)
-{printf("Block no.%d: ", i);
-scanf("%d", &b[i]);
+for(i = 1; i <= block_no; i++){
+     printf("Block no.%d: ", i);
+     scanf("%d", &blk[i]);
 }
+
 printf("\nEnter number of files: ");
-scanf("%d", &fno);
+scanf("%d", &fl_no);
 printf("Enter size of files\n");
-for (i = 1; i <= fno; i++)
-{
+for (i = 1; i <= fl_no; i++){
 printf("File no.%d: ", i);
-scanf("%d", &f[i]);
+scanf("%d", &fl[i]);
 }
-for (i = 1; i <= fno; i++)
-{
-for (j = 1; j <= bno; j++)
-{
-if (barr[j] != 1){
-temp = b[j] - f[i];
+
+for(i = 1; i <= fl_no; i++){
+    for(j = 1; j <= block_no; j++){
+        if(barr[j] != 1){
+          temp = blk[j] - fl[i];
 if (temp >= 0)
-if (lowest > temp){
-farr[i] = j;
-lowest = temp;
+    if (lowest > temp){
+     blk_no[i] = j;
+     lowest = temp;
+     }
+  }
 }
-}
-}
-barr[farr[i]] = 1;
+barr[blk_no[i]] = 1;
 lowest = 10000;
 }
+
 printf("\nFile_no\t\tFile_size\tBlock_no\tBlock_size");
-for (i = 1; i <= fno && farr[i] != 0; i++){
-     printf("\n%d\t\t%d\t\t%d\t\t%d", i, f[i], farr[i], b[farr[i]]);
+for (i = 1; i <= fl_no && blk_no[i] != 0; i++){
+     printf("\n%d\t\t%d\t\t%d\t\t%d", i, fl[i], blk_no[i], blk[blk_no[i]]);
 }
 printf("\n");
 }
-
