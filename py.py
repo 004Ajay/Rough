c73ed34e599c4d014c7c096e2235f94ec9f95bd8
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-18 23:20:03 +0530

    new code

--- py.py
+++ py.py
@@ -1,5 +1,12 @@
 import random
 
-def sums(num):
-    for i in range(20):
-        print(f"{random.randint(1, i)} + {random.randint(1, i)}")
+def init():
+    i = 10
+    print(f"What is {random.randint(1, i)} + {random.randint(1, i)} ?")
+
+def getAns():
+    ans = int(input("Your opinion: "))
+    if ans == random.randint(1, i) + random.randint(1, i):
+        print("ANSWER IS CORRECT")
+    else:
+        print("ANSWER IS WRONG")
