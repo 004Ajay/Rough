2059caba2509a2de55ea58e95370d3f953ee69f1
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-20 20:20:33 +0530

    new code

--- For OS/SCAN_ToLow.c
+++ For OS/SCAN_ToLow.c
@@ -11,7 +11,6 @@ int main(){
     scanf("%d",&head);
     printf("Enter total disk size: ");
     scanf("%d",&size);
-    
     // Sorting using bubble sort (short job first)
     for(i=0;i<n;i++){
         for(j=0;j<n-i-1;j++){
@@ -23,54 +22,27 @@ int main(){
             }
         }
     }
-
     int index;
-    for(i=0;i<n;i++)
-    {
-        if(head<req[i])
-        {
+    for(i=0;i<n;i++){
+        if(head<req[i]){
             index=i;
             break;
         }
     }
-   
     // if movement is towards high value
-    if(move==1)
-    {
-        for(i=index;i<n;i++)
-        {
+    if(move==1){
+        for(i=index;i<n;i++){
             dist=dist+abs(req[i]-head);
             head=req[i];
         }
         //  last movement for max size 
         dist=dist+abs(size-req[i-1]-1);
         head = size-1;
-        for(i=index-1;i>=0;i--)
-        {
+        for(i=index-1;i>=0;i--){
              dist=dist+abs(req[i]-head);
              head=req[i];
-            
         }
     }
-    // if movement is towards low value
-    else
-    {
-        for(i=index-1;i>=0;i--)
-        {
-            dist=dist+abs(req[i]-head);
-            head=req[i];
-        }
-        //  last movement for min size 
-        dist=dist+abs(req[i+1]-0);
-        head =0;
-        for(i=index;i<n;i++)
-        {
-             dist=dist+abs(req[i]-head);
-             head=req[i];
-            
-        }
-    }
-    
-    printf("Total head movement is %d",dist);
+    printf("Total Distance: %d", dist);
     return 0;
 }
