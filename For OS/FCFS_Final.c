b2abea184c836412160cc6b3375c832c6e36e732
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-21 00:10:34 +0530

    Final Code

--- For OS/FCFS_Final.c
+++ For OS/FCFS_Final.c
@@ -1,42 +1,21 @@
 #include<stdio.h>
 
 int main(){
-    int reqArr[20], n, start, i, pos, res=0, j, temp;
+    int i, req_num, req[50], mov=0, head;
     printf("Enter number of requests: ");
-    scanf("%d", &n);
-    printf("Enter requests: ");
-    for(i=0;i<n;i++)
-        scanf("%d", &reqArr[i]);
+    scanf("%d", &req_num);
+    printf("Enter request order: ");
+    for(i = 0; i < req_num; i++)
+        scanf("%d", &req[i]);
     printf("Enter head position: ");
-    scanf("%d", &start);
-    //sorting
-    for(i=0;i<n-1;i++) {
-        for(j=0;j<n-1-i;j++) {
-            if(reqArr[j]>reqArr[j+1]) {
-                temp = reqArr[j];
-                reqArr[j] = reqArr[j+1];
-                reqArr[j+1] = temp;
-            }
-        }
+    scanf("%d", &head);
+    mov = mov + abs(head - req[0]); // abs() - to calculate absolute value
+    printf("%d -> %d", head, req[0]);
+    for(i = 1; i < req_num; i++){
+        mov = mov + abs(req[i]-req[i-1]);
+        printf(" -> %d", req[i]);
     }
-    /*
-    // position of the disk to start seeking 
-    for(i=0;i<n;i++) {                                      
-        if(reqArr[i] == start) {
-            pos = i;
-            break;
-        }
-    }
-    */
-    res = abs(start - reqArr[0]);
-    printf("%d -> %d", start, reqArr[1]);
-    for(i = 0; i < n-1; i++){
-        res += abs(reqArr[i+1] - reqArr[i]); 
-        if(i == 1)
-           continue;
-        else
-            printf(" -> %d", reqArr[i]);
-    }
-    printf("\nTotal Distance: %d\n", res);
+    printf("\n");
+    printf("Total Distance: %d\n",mov);
     return 0;
 }
