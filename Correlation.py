25400a5d59c468eca81085570a1b304dbcb80d4a
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-22 21:32:31 +0530

    changes

--- Correlation.py
+++ Correlation.py
@@ -24,13 +24,13 @@ Xi_sq_sum = sum(Xi_sq)
 Yi_sq_sum = sum(Yi_sq)
 
 Pxy = ((1/n)*XiYi) - xy_bar
-sig_x = statistics.sqrt(((1/n)*Xi_sq_sum) - x_mean)
-sig_y = statistics.sqrt(((1/n)*Yi_sq_sum) - y_mean)
+sig_x = math.sqrt(((1/n)*Xi_sq_sum) - x_mean)
+sig_y = math.sqrt(((1/n)*Yi_sq_sum) - y_mean)
 
 r = Pxy / (sig_x * sig_y)
 
 print("x\ty\tXiYi\tXi_sq\tYi_sq")
 for i in range(len(x)):
-    print(f"{x}\t{y}\t{XiYi}\t{Xi_sq}\t{Yi_sq}")
+    print(f"{x[i]}\t{y[i]}\t{XiYi[i]}\t{Xi_sq[i]}\t{Yi_sq[i]}")
     
 print(f"Pxy: {round(Pxy, 4)}\tsigma x: {round(sig_x, 4)}\tsigma_y: {round (sig_y, 4)}")    
