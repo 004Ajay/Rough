8745313ea2e5f775c3b52e94e4254869f5ebf95d
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-22 20:31:01 +0530

    infinite code

--- shufflesort.py
+++ shufflesort.py
@@ -4,10 +4,15 @@ steps = 0
 lst = [1,5,3,2,4]
 new_lst = []
 
-while new_lst == sorted(lst):
+while new_lst != sorted(lst):
     srt_lst = random.shuffle(lst)
-    new_lst.append(srt_lst)
-    print(lst)
-    step += 1
+    print(lst, end=" ")
+    steps += 1
+    if steps >= 100 and new_lst != sorted(lst):
+        new_lst.append("NULL")
+        break
+    else:
+        new_lst.append(srt_lst)
+        break
     
 print(f"Original list: {lst}\nSorted list: {new_lst}\nNumber of steps: {steps}")
