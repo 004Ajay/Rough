15c7c13bc3494e2000a95c694eb738800fcbbf95
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-22 22:32:00 +0530

    temp code

--- Correlation.py
+++ Correlation.py
@@ -6,31 +6,36 @@ import statistics
 x = [10, 12, 15, 30, 25, 27, 32]
 y = [70, 2, 80, 100, 11, 16, 7]
 
-n = len(x)
-x_mean = statistics.mean(x)
-y_mean = statistics.mean(y)
-xy_bar = x_mean * y_mean
+n_div = 1/len(x)
+x_mean = round(statistics.mean(x), 4)
+y_mean = round(statistics.mean(y), 4)
+xy_bar = round(x_mean * y_mean, 4)
 
-XiYi = 0
-Xi_sq = []
-Yi_sq = []
+XiYi, Xi_sq, Yi_sq = [], [], []
+Xi_sq_sum, Yi_sq_sum, XiYi_sum = 0, 0, 0
+
+for i in range(len(x)):
+    x_mean_sq = x_mean * x_mean
+    y_mean_sq = y_mean * y_mean
 
 for i in range(len(x)):
     Xi_sq.append(x[i]*x[i])
     Yi_sq.append(y[i]*y[i])
-    XiYi += x[i] + y[i]
+
+for i in range(len(x)):    
+    XiYi.append(x[i] * y[i])
     
 Xi_sq_sum = sum(Xi_sq)
 Yi_sq_sum = sum(Yi_sq)
+XiYi_sum = sum(XiYi)
 
-Pxy = ((1/n)*XiYi) - xy_bar
-sig_x = math.sqrt(((1/n)*Xi_sq_sum) - x_mean)
-sig_y = math.sqrt(((1/n)*Yi_sq_sum) - y_mean)
+Pxy = ((n_div)*XiYi_sum) - xy_bar
+sig_x = math.sqrt(((n_div)*Xi_sq_sum) - x_mean)
+sig_y = math.sqrt(((n_div)*Yi_sq_sum) - y_mean)
+r = round(Pxy / (sig_x * sig_y), 4)
 
-r = Pxy / (sig_x * sig_y)
-
-print("x\ty\tXiYi\tXi_sq\tYi_sq")
+print(f"\n{'-'*73}\n| x\t|\ty\t|\tXiYi\t|\tXi_sq\t|\tYi_sq\t|\n{'-'*73}")
 for i in range(len(x)):
-    print(f"{x[i]}\t{y[i]}\t{XiYi[i]}\t{Xi_sq[i]}\t{Yi_sq[i]}")
-    
-print(f"Pxy: {round(Pxy, 4)}\tsigma x: {round(sig_x, 4)}\tsigma_y: {round (sig_y, 4)}")    
+    print(f"| {x[i]}\t|\t{y[i]}\t|\t{XiYi[i]}\t|\t{Xi_sq[i]}\t|\t{Yi_sq[i]}\t|\n{'-'*73}")   
+print(f"\nx̄: {x_mean}\t|\tȳ: {y_mean}\t|\tx̄ȳ: {xy_bar}\t|\tΣXiYi: {XiYi_sum}\n\nΣXi^2: {Xi_sq_sum}\t|\tΣYi^2: {Yi_sq_sum}\t|\n\n{'-'*83}")     
+print(f"\nPxy: {round(Pxy, 4)}\t|\tσx: {round(sig_x, 4)}\t|\tσy: {round (sig_y, 4)}\t|\tr: {r}\n\n{'-'*83}")
