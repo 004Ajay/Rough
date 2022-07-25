600531ccce0cf850cbb94d6b0164cdab4d1e0793
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-25 20:21:01 +0530

    new code

--- For Email/emailedit.py
+++ For Email/emailedit.py
@@ -4,22 +4,23 @@ firstHalf = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 vendor = 'gmail, icloud, github, sjcet, outlook' # just a sample items
 domain = 'com, in, ac, ac.in, net'
 
-def checkmail():
+def checkMail():
     part_one = email.split('@') # now email splitted into 'ajaytshaju', 'gmail.com' // output: ['ajaytshaju', 'gmail.com']
     part_two = part_one[1].split('.') # now 'gmail.com' becomes 'gmail', 'com' // output: ['gmail', 'com']
 
     for i in part_one[0]: # checking first half i.e 'ajaytshaju'
-        print("Invalid Email, Problem in first half") if i not in firstHalf else return 0
-        
+        if i not in firstHalf:
+            print("Problem in first half.", end=" ")
+            return 0
 
     for j in part_two[0]: # checking middle half of 'gmail.com' which is 'gmail'
         if j not in vendor:
-            print("Invalid Email, Problem in middle half")
+            print("Problem in middle half.", end=" ")
             return 0
 
     for k in part_two[1]: # checking second half of 'gmail.com' which is 'com'
         if k not in domain:
-            print("Invalid Email, Problem in last half")
+            print("Problem in last half.", end=" ")
             return 0
     return 1 # no errors found in the input email                  
 
@@ -28,6 +29,8 @@ while True: # for recurrent inputting
     
     if "@" in email: # checking for '@'
         if "." in email: # checking for '.'
-            print("Valid Email") if checkmail() else None  # passes every checking, if-else shorthand used  
-    if input("Again? y/n ") == 'n':
+            print("Valid Email") if checkMail() else print("So, Invalid Email")  # passes every checking, if-else shorthand used
+        else: print("No '.' between vendor name & domain")
+    else: print("No '@' after first part")        
+    if input("Again? y/n: ") == 'n':
         break
