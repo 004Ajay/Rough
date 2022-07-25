f9d219da8e1edd6455b4e06783ef34362bc8e8d1
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-25 20:06:37 +0530

    new

--- For Email/emailedit.py
+++ For Email/emailedit.py
@@ -1,40 +1,33 @@
-# PYTHON PROGRAM TO CHECK THE VALIDITY OF AN EMAIL BY VERIFYING ITS FORMAT
+# PYTHON PROGRAM TO CHECK THE VALIDITY OF AN EMAIL BY VERIFYING IT'S FORMAT
 
-first_half = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-middle_half = 'gmail, hotmail, yahoo, icloud, github, sjcet, outlook'
-last_half = 'com, in, ac, ac.in, net'
+firstHalf = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+vendor = 'gmail, icloud, github, sjcet, outlook' # just a sample items
+domain = 'com, in, ac, ac.in, net'
 
-def checkmail(): # return value: 1 == error, 0 = no error
-
-    part_one = email.split('@') # now email splitted into 'ajaytshaju', 'gmail.com'
-    part_two = part_one[1].split('.') # now 'gmail.com' becomes 'gmail', 'com'
+def checkmail():
+    part_one = email.split('@') # now email splitted into 'ajaytshaju', 'gmail.com' // output: ['ajaytshaju', 'gmail.com']
+    part_two = part_one[1].split('.') # now 'gmail.com' becomes 'gmail', 'com' // output: ['gmail', 'com']
 
     for i in part_one[0]: # checking first half i.e 'ajaytshaju'
-        if i not in first_half:
-            print("Invalid Email, Problem in first half")
-            return 1
+        print("Invalid Email, Problem in first half") if i not in firstHalf else return 0
+        
 
     for j in part_two[0]: # checking middle half of 'gmail.com' which is 'gmail'
-        if j not in middle_half:
+        if j not in vendor:
             print("Invalid Email, Problem in middle half")
-            return 1
+            return 0
 
     for k in part_two[1]: # checking second half of 'gmail.com' which is 'com'
-        if k not in last_half:
+        if k not in domain:
             print("Invalid Email, Problem in last half")
-            return 1
+            return 0
+    return 1 # no errors found in the input email                  
 
-    return 0 # no errors found in the input email                  
-
-while True:
-    email = input("Enter email: ") # getting email, eg - ajaytshaju@gmail.com
+while True: # for recurrent inputting
+    email = input("Enter an Email: ") # getting email, eg - ajaytshaju@gmail.com
     
     if "@" in email: # checking for '@'
         if "." in email: # checking for '.'
-            value = checkmail()
-            if value == 0: # if no error, 1 won't be returned
-                print("Valid Email") # it passes every levels of checking
-            elif value == 1:
-                None    
+            print("Valid Email") if checkmail() else None  # passes every checking, if-else shorthand used  
     if input("Again? y/n ") == 'n':
         break
