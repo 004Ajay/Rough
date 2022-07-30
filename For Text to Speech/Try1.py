011812e65d90d82c6073ac794c9ac5eb699b6a14
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-30 21:41:27 +0530

    working code

--- For Text to Speech/Try1.py
+++ For Text to Speech/Try1.py
@@ -10,13 +10,14 @@ sen = input("Enter the sentence:\n") # getting text from user for conversion to 
 # text -> pass your sentence
 # lang -> your preferred language
 # slow -> speed of output audio, pass as true or false
-myobj = gt(text = sen, lang = 'en', slow = True)
+toAudio = gt(text = sen, lang = 'en', slow = True)
 
 try:
-    # Saving the converted audio in a mp3 file
-    myobj.save("new.mp3")
-    print("File saved to Desktop")
-    # Playing the converted file
-    os.system("new.mp3")
+    toAudio.save("new.mp3")  # Saving the converted audio in a mp3 file
+    main =  open("new.mp3", "rb").read() # opening saved mp3 file for copying to another location
+    with open('C:/Users/ASUS/Desktop/audio.mp3',  'wb+') as dest: # change path as your need
+        dest.write(main)
+    print("File saved to Desktop") 
+    os.system("new.mp3") # playing the text inside program
 except:
     print("Unable to process")
