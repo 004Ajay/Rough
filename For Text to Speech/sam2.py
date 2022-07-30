dfa9b74aaa7f5ec1b4b000e257e5ed61788af6fc
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-30 21:41:56 +0530

    name change

--- For Text to Speech/sam2.py
+++ For Text to Speech/sam2.py
@@ -1,15 +1,25 @@
+# Make a Text to Speech Software in python with 
+# Output Audio Speed Control & Voice Recognition from user 
+# V_Recog -> like Mm.. Yes..
+
+from gtts import gTTS as gt
 import os
 
-save_path = 'D:\Temp'
+sen = input("Enter the sentence:\n") # getting text from user for conversion to audio
 
-name_of_file = input("What is the name of the file: ")
+# text -> pass your sentence
+# lang -> your preferred language
+# slow -> speed of output audio, pass as true or false
+toAudio = gt(text = sen, lang = 'en', slow = True)
 
-completeName = os.path.join(save_path, name_of_file+".txt")         
+try:
+    toAudio.save("new.mp3")  # Saving the converted audio in a mp3 file
+    main =  open("new.mp3", "rb").read() # opening saved mp3 file for copying to another location
+    with open('C:/Users/ASUS/Desktop/audio.mp3',  'wb+') as dest: # change path as your need
+        dest.write(main)
+    print("File saved to Desktop") 
+    os.system("new.mp3") # playing the text inside program
+except:
+    print("Unable to process")
 
-file1 = open(completeName, "w")
 
-toFile = input("Write what you want into the field")
-
-file1.write(toFile)
-
-file1.close()
