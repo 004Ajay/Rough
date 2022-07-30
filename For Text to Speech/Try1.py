f879363db630cad86779c3307a22db1447cfce66
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-30 19:47:56 +0530

    new

--- For Text to Speech/Try1.py
+++ For Text to Speech/Try1.py
@@ -2,24 +2,21 @@
 # Output Audio Speed Control & Voice Recognition from user 
 # V_Recog -> like Mm.. Yes..
 
-"""
-
-"""
-
 from gtts import gTTS as gt
 import os
-
-from sqlalchemy import true
 
 sen = input("Enter the sentence:\n") # getting text from user for conversion to audio
 
 # text -> pass your sentence
 # lang -> your preferred language
 # slow -> speed of output audio, pass as true or false
-myobj = gt(text = sen, lang = 'en', slow = true)
+myobj = gt(text = sen, lang = 'en', slow = True)
 
-# Saving the converted audio in a mp3 file
-myobj.save("welcome.mp3")
-
-# Playing the converted file
-os.system("welcome.mp3")
+try:
+    # Saving the converted audio in a mp3 file
+    myobj.save("welcome.mp3")
+    print("File saved to Desktop")
+    # Playing the converted file
+    os.system("welcome.mp3")
+except:
+    print("Unable to process")
