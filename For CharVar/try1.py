5f9b36804c8a0fa064eb20268f7f31b878c8edc3
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-30 11:19:31 +0530

    new program

--- For CharVar/try1.py
+++ For CharVar/try1.py
@@ -1,1 +1,4 @@
 # This program convert your word to number which belogs to alphabets
+
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] # a to z
+alpah_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26] # 1 to 26
