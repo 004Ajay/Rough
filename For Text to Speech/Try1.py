9bc397beea00db9475eaf05c6ad881518094dac3
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-30 19:09:14 +0530

    new program

--- For Text to Speech/Try1.py
+++ For Text to Speech/Try1.py
@@ -1,3 +1,25 @@
 # Make a Text to Speech Software in python with 
 # Output Audio Speed Control & Voice Recognition from user 
 # V_Recog -> like Mm.. Yes..
+
+"""
+
+"""
+
+from gtts import gTTS as gt
+import os
+
+from sqlalchemy import true
+
+sen = input("Enter the sentence:\n") # getting text from user for conversion to audio
+
+# text -> pass your sentence
+# lang -> your preferred language
+# slow -> speed of output audio, pass as true or false
+myobj = gt(text = sen, lang = 'en', slow = true)
+
+# Saving the converted audio in a mp3 file
+myobj.save("welcome.mp3")
+
+# Playing the converted file
+os.system("welcome.mp3")
