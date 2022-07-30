779644f955d16e41a5250782cc5c7d75fb743f04
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-30 11:39:32 +0530

    changes

--- For CharVar/try1.py
+++ For CharVar/try1.py
@@ -1,10 +1,32 @@
 # This program convert your word to number which belogs to alphabets
 
+from traceback import print_tb
+
+
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] # a to z
-alpah_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26] # 1 to 26
+alpha_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26] # 1 to 26
 
-word = input("Enter the word: ").lower()
+dic = dict(zip(alpha, alpha_num))
+print(dic)
 
-print(tuple(zip(alpha, alpah_num)))
+
+
+"""
+
+lst = []
+
+for i in range(len(alpha)):
+    res = { alpha[i] : alpha_num[i] }
+    lst.append(res)
+
+print(lst)
+
+"""
+
+
+
+# word = input("Enter the word: ").lower()
+
+# print(tuple(zip(alpha, alpha_num)))
 
 # for i in word:
