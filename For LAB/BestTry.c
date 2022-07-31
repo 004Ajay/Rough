e6df7c8ef99d3936057ad6e18df0f6a5fb0b228e
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-31 19:25:47 +0530

    changes

--- For LAB/BestTry.c
+++ For LAB/BestTry.c
@@ -2,8 +2,8 @@
 
 int main() {
     int alloc[20], i, j, temp = 0;; 
-    int blocks[] = {10, 23, 21, 43, 65, 55, 47};
-    int prcs[] = {1, 13, 11, 41, 63, 51, 37};
+    int blocks[] = {20, 50, 40, 30, 80};
+    int prcs[] = {35, 45, 25};
     int blk_n = sizeof(blocks) / sizeof(blocks[0]);
     int prcs_n = sizeof(prcs) / sizeof(prcs[0]);
     
@@ -17,16 +17,14 @@ int main() {
         }
     }
     
-    for(i=0; i<blk_n; i++)
-        alloc[i] = blocks[i];
+    // for(i=0; i<blk_n; i++)
+        
     
     for(i=0; i<blk_n; i++){
+        alloc[i] = blocks[i];
         if(prcs[i] > blocks[i])
            blocks[i] = -1;
     }
-    
-    for(i=0; i<7; i++)
-        printf("blocks[%d] : %d\n", i+1, blocks[i]);
     printf("\n\nBlocks Size\t\tProcess\t\t\tAllocated Blocks\n");
     for(j=0; j<blk_n; j++)
         printf("%d\t\t\t\t%d\t\t\t\t%d\n", alloc[j], prcs[j], blocks[j]);
