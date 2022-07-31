f8e04169feebf79cfa00b58c4250ad0656028919
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-31 15:47:35 +0530

    new codes

--- For LAB/worst.c
+++ For LAB/worst.c
@@ -1,41 +1,34 @@
-#include<stdio.h>
-#define max 25
+#include <stdio.h>
 
-void main(){
-int block_sz[max], prcs_sz[max], bf[max],ff[max], i, j, num_of_blocks, num_of_prcs, temp, highest=0;
-printf("\nMemory Management Scheme - Worst Fit\n");
-printf("\nEnter number of blocks: ");
-scanf("%d",&num_of_blocks);
-printf("Enter size of blocks:\n");
-for(i = 1; i <= num_of_blocks; i++){
- printf("Block %d: ", i);
- scanf("%d",&block_sz[i]);
-}
-
-printf("Enter number of Processes: ");
-scanf("%d", &num_of_prcs);
-printf("Enter size of Processes:\n");
-for(i = 1; i <= num_of_prcs; i++){
- printf("Process %d: ", i);
- scanf("%d", &prcs_sz[i]);
-}
-
-for(i = 1; i <= num_of_prcs; i++){
-	for(j = 1; j <= num_of_blocks; j++){
-		if(bf[j] != 1){
-			temp = block_sz[j] - prcs_sz[i];
-			if(highest < temp){
-				ff[i] = j;
-				highest = temp;
-			}
-		}
-	}
-	bf[ff[i]] = 1;
-	highest = 0;
-}
-
-printf("\nProcess No  \tProcess Size  \tBlocks");
-for(i = 1; i <= num_of_prcs; i++)
-    printf("\n%d\t\t%d\t\t%d", i-1, prcs_sz[i], block_sz[ff[i]]);
-printf("\n");
+int main() {
+    int alloc[20], i, j, temp = 0;; 
+    int blocks[] = {10, 23, 21, 43, 65, 55, 47};
+    int prcs[] = {1, 13, 11, 41, 63, 51, 37};
+    int blk_n = sizeof(blocks) / sizeof(blocks[0]);
+    int prcs_n = sizeof(prcs) / sizeof(prcs[0]);
+    
+    for(i = 0; i < blk_n; i++){
+        for(j = 0; j < blk_n-i-1; j++){
+            if(blocks[j] < blocks[j+1]){
+                temp = blocks[j];
+                blocks[j] = blocks[j+1];
+                blocks[j+1] = temp;
+            }
+        }
+    }
+    
+    for(i=0; i<blk_n; i++)
+        alloc[i] = blocks[i];
+    
+    for(i=0; i<blk_n; i++){
+        if(prcs[i] > blocks[i])
+           blocks[i] = -1;
+    }
+    
+    for(i=0; i<7; i++)
+        printf("blocks[%d] : %d\n", i+1, blocks[i]);
+    printf("\n\nBlocks Size\t\tProcess Size\tAllocated Blocks\n");
+    for(j=0; j<blk_n; j++)
+        printf("%d\t\t\t\t%d\t\t\t\t%d\n", alloc[j], prcs[j], blocks[j]);
+    return 0;
 }
--- For LAB/worstTry.c
+++ For LAB/worstTry.c
@@ -1,46 +1,34 @@
 #include <stdio.h>
 
-int i, j, temp = 0;
-
-int sort(int arr[], int n){
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n-i-1; j++){
-            if(arr[j] < arr[j+1]){
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-}
-
 int main() {
-    int alloc[20]; 
+    int alloc[20], i, j, temp = 0;; 
     int blocks[] = {10, 23, 21, 43, 65, 55, 47};
     int prcs[] = {1, 13, 11, 41, 63, 51, 37};
     int blk_n = sizeof(blocks) / sizeof(blocks[0]);
     int prcs_n = sizeof(prcs) / sizeof(prcs[0]);
     
-    sort(blocks, blk_n);
-    
-    for(i=0; i<blk_n; i++){
-        alloc[i] = blocks[i];
+    for(i = 0; i < blk_n; i++){
+        for(j = 0; j < blk_n-i-1; j++){
+            if(blocks[j] < blocks[j+1]){
+                temp = blocks[j];
+                blocks[j] = blocks[j+1];
+                blocks[j+1] = temp;
+            }
+        }
     }
+    
+    for(i=0; i<blk_n; i++)
+        alloc[i] = blocks[i];
     
     for(i=0; i<blk_n; i++){
         if(prcs[i] > blocks[i])
            blocks[i] = -1;
-        
     }
     
-    
-    for(i=0; i<7; i++){
-    printf("blocks[%d] : %d\n", i+1, blocks[i]);
-    }
-    
+    for(i=0; i<7; i++)
+        printf("blocks[%d] : %d\n", i+1, blocks[i]);
     printf("\n\nBlocks Size\t\tProcess\t\t\tAllocated Blocks\n");
-    for(j=0; j<blk_n; j++){
+    for(j=0; j<blk_n; j++)
         printf("%d\t\t\t\t%d\t\t\t\t%d\n", alloc[j], prcs[j], blocks[j]);
-    }
     return 0;
 }
