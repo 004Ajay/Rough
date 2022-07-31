d28695ff2c901a916aa786b41bcf8d5842cbb7f9
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-31 15:06:00 +0530

    new code

--- For LAB/worstTry.c
+++ For LAB/worstTry.c
@@ -15,16 +15,32 @@ int sort(int arr[], int n){
 }
 
 int main() {
-    
+    int alloc[20]; 
     int blocks[] = {10, 23, 21, 43, 65, 55, 47};
     int prcs[] = {1, 13, 11, 41, 63, 51, 37};
     int blk_n = sizeof(blocks) / sizeof(blocks[0]);
-    int blk_n = sizeof(prcs) / sizeof(prcs[0]);
+    int prcs_n = sizeof(prcs) / sizeof(prcs[0]);
     
-    sort(blocks, n);
+    sort(blocks, blk_n);
+    
+    for(i=0; i<blk_n; i++){
+        alloc[i] = blocks[i];
+    }
+    
+    for(i=0; i<blk_n; i++){
+        if(prcs[i] > blocks[i])
+           blocks[i] = -1;
+        
+    }
+    
     
     for(i=0; i<7; i++){
-    printf("blocks [%d] : %d\n", i+1, blocks[i]);
+    printf("blocks[%d] : %d\n", i+1, blocks[i]);
+    }
+    
+    printf("\n\nBlocks Size\t\tProcess\t\t\tAllocated Blocks\n");
+    for(j=0; j<blk_n; j++){
+        printf("%d\t\t\t\t%d\t\t\t\t%d\n", alloc[j], prcs[j], blocks[j]);
     }
     return 0;
 }
