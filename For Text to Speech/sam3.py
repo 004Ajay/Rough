2b80055b2bd53bbe66ecd99c76e7bc207ccca12e
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-01 14:23:08 +0530

    test

--- For Text to Speech/sam3.py
+++ For Text to Speech/sam3.py
@@ -12,19 +12,31 @@ root = Tk()
 root.geometry("300x300")
 root.title("Text to Speech")
 
-def input():
-	inp = inputtxt.get("1.0", "end-1c")
-	print(inp)
-	if(inp == "120"):
-		Output.insert(END, 'Correct')
-	else:
-		Output.insert(END, "Wrong answer")
-	
 lb = Label(text = "Enter your text here:") # heading
+inputtxt = Text(root, height = 10, width = 25, bg = "white")
+Output = Text(root, height = 3, width = 30, bg = "White") # Output screen
 
-inputtxt = Text(root, height = 10, width = 25, bg = "light yellow")
+def input():
+	inp = inputtxt.get("1.0", "end-1c") # getting text from user for conversion to audio
+    sen = inp
+	print(sen)
+    # text -> pass your sentence
+    # lang -> your preferred language
+    # slow -> speed of output audio, pass as true or false
+toAudio = gTTS(text = sen, lang = 'en', slow = True) # converting text to audio
+    
+    try:
+        toAudio.save("new.mp3")  # Saving the converted audio in a mp3 file
+        main =  open("new.mp3", "rb").read() # opening saved mp3 file for copying to another location
+        with open('C:/Users/ASUS/Desktop/audio.mp3',  'wb+') as dest: # change path as your need
+            dest.write(main)
+        print("File saved to Desktop") 
+        os.system("new.mp3") # playing the text inside program
+        Output.insert(END, 'Working on it')
+    except:
+        Output.insert(END, "Unable to process")
+	
 
-Output = Text(root, height = 5, width = 5, bg = "light cyan") # Output screen
 
 Display = Button(root, height = 2, width = 20, text = "Convert", command = lambda:input())
 
