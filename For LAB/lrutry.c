44519ce5a1ba76f6a133c9d14984ead4fafac037
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-01 13:09:34 +0530

    changes

--- For LAB/lrutry.c
+++ For LAB/lrutry.c
@@ -4,25 +4,23 @@
 int checkHit(int incomingPage, int queue[], int occupied){
     
     for(int i = 0; i < occupied; i++){
-        if(incomingPage == queue[i])
-            return 1;
+        if(incomingPage == queue[i]) return 1;
+        return 0;
     }
-    
-    return 0;
-}
+}    
 
 void printFrame(int queue[], int occupied)
 {
     for(int i = 0; i < occupied; i++)
-        printf("%d\t\t\t",queue[i]);
+        printf("%d\t\t",queue[i]);
 }
 
 int main()
 {
 
 //    int incomingStream[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1};
-//    int incomingStream[] = {1, 2, 3, 2, 1, 5, 2, 1, 6, 2, 5, 6, 3, 1, 3, 6, 1, 2, 4, 3};
-    int incomingStream[] = {1, 2, 3, 2, 1, 5, 2, 1, 6, 2, 5, 6, 3, 1, 3};
+    int incomingStream[] = {1, 2, 3, 2, 1, 5, 2, 1, 6, 2, 5, 6, 3, 1, 3, 6, 1, 2, 4, 3};
+//    int incomingStream[] = {1, 2, 3, 2, 1, 5, 2, 1, 6, 2, 5, 6, 3, 1, 3};
     
     int n = sizeof(incomingStream)/sizeof(incomingStream[0]);
     int frames = 3;
@@ -31,11 +29,11 @@ int main()
     int occupied = 0;
     int pagefault = 0;
     
-    printf("Page\t Frame1 \t Frame2 \t Frame3\n");
+    printf("Page\t\tFrame1\t\tFrame2\t\tFrame3\n");
     
     for(int i = 0;i < n; i++)
     {
-        printf("%d:  \t\t",incomingStream[i]);
+        printf("%d:\t\t",incomingStream[i]);
         // what if currently in frame 7
         // next item that appears also 7
         // didnt write condition for HIT
