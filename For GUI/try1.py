d35e64452de7dd97603ee30f9353fca9e3203202
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-02 14:39:18 +0530

    new

---
+++ For GUI/try1.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=700, width=700, bg = '#0078D4') # windows blue color is used
+canvas.pack()
+
+root.mainloop()
