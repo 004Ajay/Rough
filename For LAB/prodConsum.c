0658fd2a74612bfa45476976853a876e8cbaf9f7
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-05 12:26:16 +0530

    more codes

--- For LAB/prodConsum.c
+++ For LAB/prodConsum.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 
+int signal(s){
+    s++;
+}
+
+int wait(s){
+    s--;
+} 
+
+
 int main(){
-int pro, cons, limit = 4;
+int pro, cons, s, limit = 4;
 
 int arr[limit];
-
-
 
 
 return 0;
