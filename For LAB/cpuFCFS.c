b9830aca267c77de4bb0f14b451edeaf815c2637
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-09 22:47:47 +0530

    complete

--- For LAB/cpuFCFS.c
+++ For LAB/cpuFCFS.c
@@ -3,24 +3,28 @@
 
 
 int main(){
-    int prcs, prcs_no, burstTime;
+    int prcs, burstTime, i,  prcs_no = 3;
+    int burst_time[] = {5, 8, 12};
+    int wt_time[10], tat[10];
 
-    prcs_no = 3;
+    // wt_time[0] = 0;
+    // wt time = burst time[i-1] + wt time[i-1]
+    // tat = burst time[i] + wait time[i]
 
-    int burst_time[] = {24, 3, 3};
-    int wt_time[10];
-
-
-    printf("Waiting times\n");
     wt_time[0] = 0;
 
-    
-    for (int i = 1; i < prcs_no; i++)
-        wt_time[i] = burst_time[i+1];
+    for (i = 1; i < prcs_no; i++)
+        wt_time[i] = burst_time[i-1] + wt_time[i-1];
+
+    for (i = 0; i < prcs_no; i++){
+        tat[i] = burst_time[i] + wt_time[i];
+        printf("%d\n", tat[i]);
+    }
     
 
-    for (int i = 0; i < prcs_no; i++)
-        printf("%d\n", wt_time[i]);
+    printf("Turn Around times\tWaiting times\n");
+    //for (int i = 0; i < prcs_no; i++)
+     //   printf("%d\n", wt_time[i]);
 
     return 0;
 }
