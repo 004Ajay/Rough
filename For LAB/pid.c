5025aa064340f7f626b02b82e6fcc43b9b84c37f
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-09 19:50:03 +0530

    changes

--- For LAB/pid.c
+++ For LAB/pid.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
-
+#include<sys/types.h>
+// #include<stdlib.h>
+// #include<sys/wait.h>
+#include<unistd.h>
 
 int main(){
     printf("Your process id: %d", getpid());
+
+    if (fork() != 0)
+        printf("Success");
+    else
+    printf("No Success");    
+
+
     return 0;
 }
