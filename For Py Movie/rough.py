5776b7fab4680a5fd34c869a7d24fc7a952d922d
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-12 09:45:17 +0530

    change

---
+++ For Py Movie/rough.py
@@ -0,0 +1,14 @@
+lst = []
+
+r, h = [], []
+
+wr = "ajey"
+
+for i in wr:
+    print(i)
+    if i == 'a':
+        r.append(i)
+    else:
+        h.append(i)
+
+print(f"r list: {r}\nh list: {h}\n")
