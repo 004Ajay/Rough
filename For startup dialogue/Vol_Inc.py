1da50799df4f9fdeaa4f6e649b5b1c7b22df3165
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-14 17:37:25 +0530

    final code

---
+++ For startup dialogue/Vol_Inc.py
@@ -0,0 +1,11 @@
+import win32api
+import win32gui
+
+WM_APPCOMMAND = 0x319
+
+for i in range(15): # running 15 times means increasing volume by 30 points, i.e, (no_of_times * 2 vol_points)
+    vol_inc = 0xA0000 # Virtual-Key Code for increasing volume
+    hwnd_active = win32gui.GetForegroundWindow() # Getting active window
+    win32api.SendMessage(hwnd_active, WM_APPCOMMAND, None, vol_inc) # Executing command
+    # APPCOMMAND_VOLUME_MUTE(8) (0xFFFF + 1) * 8 = 0x80000
+    # APPCOMMAND_VOLUME_UP (10) (0xFFFF + 1) * 10 = 0xA0000
