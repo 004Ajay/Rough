82f2fb6adde36b21f9c4b82d279ede00139f8d86
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-14 13:08:36 +0530

    new lines

--- For OpenCV/first.py
+++ For OpenCV/first.py
@@ -1,8 +1,12 @@
 import cv2
 import numpy as np
 
-inp = cv2.imread('image.jpg')
+inp = cv2.imread('img.jpg')
 
 out = cv2.imshow('AJAY', inp)
 
-print(inp.shape)
+cv2.waitKey()
+
+print(f"Height: {inp.shape[0]}\nWidth: {inp.shape[1]}\nChannels: {inp.shape[2]}\n") # Image informations
+
+cv2.destroyAllWindows()
