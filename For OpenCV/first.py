e216f6a84ac885f35775c754e78ace07f87b0339
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-14 13:16:56 +0530

    more lines

--- For OpenCV/first.py
+++ For OpenCV/first.py
@@ -1,12 +1,17 @@
 import cv2
 import numpy as np
 
-inp = cv2.imread('img.jpg')
+inp = cv2.imread('img.jpg', 1) # opening image, num 1 indicates 'Color Image', 0 means 'B&W Image'
 
-out = cv2.imshow('AJAY', inp)
+out = cv2.imshow('Test Image', inp) # showing image with title as 1st argument
 
-cv2.waitKey()
+cv2.waitKey() # wait time in ms, until a key-press detected
 
-print(f"Height: {inp.shape[0]}\nWidth: {inp.shape[1]}\nChannels: {inp.shape[2]}\n") # Image informations
+print("\nshape: ", inp.shape)
+
+try:
+    print(f"Height: {inp.shape[0]}\nWidth: {inp.shape[1]}\nChannels: {inp.shape[2]}\n") # Image informations
+except IndexError:
+    print("\nIndex out of range error for displaying image information.\n")
 
 cv2.destroyAllWindows()
