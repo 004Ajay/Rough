98405f2ee9b856cebb6d9b76415f94edefd02810
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-14 13:24:59 +0530

    final

--- For OpenCV/first.py
+++ For OpenCV/first.py
@@ -1,17 +1,16 @@
 import cv2
-import numpy as np
 
-inp = cv2.imread('img.jpg', 1) # opening image, num 1 indicates 'Color Image', 0 means 'B&W Image'
+inp = cv2.imread('img.jpg', 0) # opening image, num 1 indicates 'Color Image', 0 means 'B&W Image'
 
 out = cv2.imshow('Test Image', inp) # showing image with title as 1st argument
 
 cv2.waitKey() # wait time in ms, until a key-press detected
 
-print("\nshape: ", inp.shape)
+print(f"\n\Height: {inp.shape[0]}\nWidth: {inp.shape[1]}") # Image informations
 
 try:
-    print(f"Height: {inp.shape[0]}\nWidth: {inp.shape[1]}\nChannels: {inp.shape[2]}\n") # Image informations
-except IndexError:
-    print("\nIndex out of range error for displaying image information.\n")
+    print(f"\nChannels: {inp.shape[2]}\n") # Image channels
+except IndexError:    
+    print("Error displaying image channels\n") # Greyscale images has default channel
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() # Allows users to destroy all windows after exiting script.
