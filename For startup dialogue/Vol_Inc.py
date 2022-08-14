2ebf71ee4b815adeff8694a05759fc4d92c1d1ed
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-14 17:38:01 +0530

    final

--- For startup dialogue/Vol_Inc.py
+++ For startup dialogue/Vol_Inc.py
@@ -9,3 +9,4 @@ for i in range(15): # running 15 times means increasing volume by 30 points, i.e
     win32api.SendMessage(hwnd_active, WM_APPCOMMAND, None, vol_inc) # Executing command
     # APPCOMMAND_VOLUME_MUTE(8) (0xFFFF + 1) * 8 = 0x80000
     # APPCOMMAND_VOLUME_UP (10) (0xFFFF + 1) * 10 = 0xA0000
+    
