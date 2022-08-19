0cacb87de75d5dbe8e2fb8463cb816624ea1be0d
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-19 14:28:43 +0530

    new

---
+++ rough.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import vlc
+
+
+window = Tk()
+
+# window title
+window.title("Test")
+
+window.configure(width=800, height=500)
+window.configure(bg='#430752')
+window.mainloop()
