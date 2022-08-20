af353c619ff2bd76a99070605b237efe2b8489b6
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-20 13:29:49 +0530

    new

--- rough.py
+++ rough.py
@@ -1,52 +1,47 @@
-whitelist = [{'name':'str_1','place':'plc_1','rail':['private','protected'],'port':[100,101,102,103]}, {'name':'str_2','place':'plc_2','rail':['protected'],'port':[201,202]}, {'name':'str_3','place':'plc_3','rail':['private'],'port':[303]}]
+def q7_assess_bolt(strength, diameter_error, amart_near):
+    '''
+    Returns one of three string messages depending on whether a
+    bolt is of acceptable quality or not.
+    Parameters:
+    strength - the bolt's tensile strength in MPa
+    diameter_error - the bolt's diameter variation in micrometres
+    amart_near - True iff there is an Amart Furniture near the factory
 
-for index, row in df.iterrows():
+    A bolt must have a tensile strength of at least 400 MPa to pass A bolt that is strong enough must 
+    also have a diameter variation of no more than 75 micrometres in order to pass
 
-for wl_list in whitelist:
-    if wl_list['name'] in row['name']:
-        variable = request.get("link",headers=headers).json()
-    else:
-        variable = None
+    If a bolt doesn’t pass, but has a diameter variation less than 125 micrometres, and 
+    there is an Amart Furniture nearby, then the company will sell it to Amart Furniture instead of discarding it
 
-if row['services'] !=0 :
-    for t_service in row['services']:
-        if wl_list['name'] != 'global' and variable != None:
-            for var in variable :
-                if (var['status']== 'active' and var['id'] in service['id']) and ('private' in var['rail'] or 'protected' in var['rail'] ):
-                    if wl_list['place'] in var['place']:
-                        for wl_rail in wl_list['rail']:
-                            if wl_rail in var['rail'] and 'ports' in var.keys():
-                                for v_port in var['ports']:
-                                    if len(v_port) != 0:
-                                        if v_port['from'] == v_port['to']:
+    Any bolt that does not pass and is not sold to Amart Furniture is rejected 
 
-                                            if 'private' in wl_rail:
-                                                if v_port['from'] in wl_list['port']:
-                                                    n_private = 0
-                                                else:
-                                                    n_private = 1
-                                            if 'protected' in wl_rail:
-                                                if v_port['from'] in wl_list['port']:
-                                                    n_protected = 0
-                                                else:
-                                                    n_protected = 1
-                                        else:
-                                            for wl_port in wl_list['port']:
-                                                if wl_port > v_port['from'] and wl_port < v_port['to']:
-                                                    if 'private' in wl_rail:
-                                                if v_port['from'] in wl_list['port']:
-                                                    n_private = 0
-                                                else:
-                                                    n_private = 1
-                                            if 'protected' in wl_rail:
-                                                if v_port['from'] in wl_list['port']:
-                                                    n_protected = 0
-                                                else:
-                                                    n_protected = 1 
+    PASS, REJECT, AMART = 'Pass', 'Reject', 'Sell to Amart'
+
+    q7_assess_bolt(300, 60, False) the output is 'Sell to Amart' and not Reject like it should be
+
+    '''
+    if strength >= 400 and diameter_error <= 75:
+        return 'PASS'    
+    elif strength < 400 and diameter_error < 125 and amart_near is True:
+        return 'AMART'
+    elif strength < 400 and diameter_error <125 and amart_near is False:
+        return 'REJECT'
 
 
-                    else:
-                        if 'private' in var['rail']:
-                            n_private = 1
-                        if 'protected' in var['rail']:
-                            n_protected = 1
+    """
+    res = q7_assess_bolt(300, 60, False)
+if res: # res == True 
+    print("Pass")
+elif res == 10:# not True or not False:
+    print("Sell to Amart")    
+else: # res == False
+    print("Reject") 
+    """
+
+res = q7_assess_bolt(400, 60, False)
+if res == 'PASS': # res == True 
+    print("Pass")
+elif res == 'AMART':# not True or not False:
+    print("Sell to Amart")    
+else: # res == False
+    print("Reject")     
