4571e05baf17c5ee8ebefcc9a4f5ca6fab65b6bc
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-20 09:40:25 +0530

    finished

--- For startup dialogue/StartSpeech.c
+++
@@ -1,8 +0,0 @@
-#include <stdafx.h>
-#include <strsafe.h>
-#include <MMSystem.h>
-#pragma comment(lib, "winmm.lib")
-
-void main() {
-     PlaySound(TEXT("File path.wav"), NULL, SND_FILENAME | SND_LOOP);
-}
--- For startup dialogue/play.py
+++
@@ -1,2 +0,0 @@
-from playsound import playsound
-playsound("D:\AJAYMON\AJAY\Programming\Rough\For startup dialogue/audio.mp3")
