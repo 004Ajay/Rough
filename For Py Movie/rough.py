85c9cec2879edb11f41a32ffb5ebce17abfaceed
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-21 12:24:38 +0530

    movie list added

--- For Py Movie/rough.py
+++ For Py Movie/rough.py
@@ -1,7 +1,14 @@
 import random
 
 
-movies = ['def gh', 'kl mn']
+movies = [
+          'trance', 'forensic', 'shylock', 'the kung fu master', 'big brother', 'lucifer', 'brothers day',
+          'cold case', 'cbi', 'one', 'home', 'the priest', 'malik', 'the great indian kitchen', 'salute',
+          'joseph', 'night drive', 'traffic', 'classmates', 'bangalore days', 'sunny', 'joji', 'take off',
+          'godfather', 'ramji rao speaking', 'his highness abdullah', 'uncle bun', 'johny walker', 'hitler',
+          'superman', 'the car', 'summer in bethlehem', 'punjabi house', 'crime file', 'friends'
+         ]
+
 
 
 """
