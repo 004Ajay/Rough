526f4ede3131c0d9d56b349f0fb396bd24aa76a3
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-27 19:54:12 +0530

    del

--- WatchMovie.py
+++ WatchMovie.py
@@ -4,17 +4,16 @@ movies = [
 "13 lives",
 "Around the World in 80 Days",
 "Bro Daddy",
-"Mike",
 "Picket 43",
 "10th class diaries",
 "Odela Railway station",
-"Sita Ramam",
-"Solomante Theneechakal",
 "Swapnakoodu",
 "Virus",
-"Naku Penta Naku Taka",
 "Aadu",
-"Utharam"
+"Utharam",
+"Mike",
+"Solomante Theneechakal",
+"Sita Ramam"
 ]
 
 print("Total number of movies: ", len(movies))
