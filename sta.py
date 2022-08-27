ab48b6b2bc8349bdd9dfaf0e199af127f5691ad7
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-27 20:32:36 +0530

    somewhat ready

--- sta.py
+++ sta.py
@@ -1,24 +1,22 @@
-from pprint import pprint
-from unicodedata import name
-
-
 stack = []
 
 def push():
-    lim = int(input("Enter limit: "))
+    lim = int(input("Enter number of element to push: "))
     for i in range(lim):
-        stack.append(input("Enter value: "))
-
-def display():
-    print(stack)
+        stack.append(int(input("Enter value: ")))
 
 def main():
     while True:
         choice = int(input("Choose any option\n1.PUSH\n2.POP\n3.DISPLAY\n4.EXIT\nChoice: "))
         if choice == 1:
             push()
+        elif choice == 2:
+            print("Stack is empty") if len(stack) == 0 else print("Popped Element: ", stack.pop())
         elif choice == 3:
-            display()
+            print("Stack is empty") if len(stack) == 0 else print(stack)
+        elif choice == 4:
+            print("Exited!\n")
+            exit()    
         if input("Continue? y/n : ") != 'y':
             break    
 
