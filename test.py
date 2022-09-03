7888fe616c1971a30f1560037fe90a8d2feaea76
Ajay T Shaju <ajaytshaju@gmail.com>
2022-09-03 11:48:56 +0530

    delete

--- test.py
+++
@@ -1,9 +0,0 @@
-while reply != 'c':
-    steps += 1 # incrementing step count
-    guess = random.randint(start, stop) # computer guessing a random num
-    reply = input(f'{guess}, low, high or correct (L, H, C)? : ').lower()
-    if reply == 'h':
-        stop = guess - 1
-    elif reply == 'l':
-        start = guess + 1
-print(f'Computer correctly guessed, {guess}\nNumber of steps: {steps}')
