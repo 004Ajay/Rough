e30907c243e703bf6e81ad553c9f643af388b04b
Ajay T Shaju <ajaytshaju@gmail.com>
2022-09-03 11:42:13 +0530

    folder removed

--- For Excel/print.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel('name.xlsx')
-
-print(df)
--- For Excel/ren.py
+++
@@ -1,4 +0,0 @@
-old_filename = input("Enter the old file name:")
-new_filename = input("Enter the new file name:")
-
-print('ren "{}" "{}"'.format(old_filename, new_filename))
