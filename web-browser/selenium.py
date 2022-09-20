dc30d73ff58232259c21c37d2430b465667111f3
Ajay T Shaju <ajaytshaju@gmail.com>
2022-09-20 08:02:07 +0530

    new loc

--- web-browser/selenium.py
+++ web-browser/selenium.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 options = Options()
-options.add_argument("user-data-dir=C:\\Users\\AtechM_03\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 2")
-driver = webdriver.Chrome(executable_path=r'C:\path\to\chromedriver.exe', chrome_options=options)
+options.add_argument("user-data-dir= C:\Program Files\Google\Chrome\Application\chrome.exe --profile-directory=Default")
+driver = webdriver.Chrome(executable_path='C:\Program Files\Google\Chrome\Application\chrome.exe', chrome_options=options)
 driver.get("https://www.google.co.in")
