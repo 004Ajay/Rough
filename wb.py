dcf253f1e4b8e377b23307d26bc400ffd0849eae
Ajay T Shaju <ajaytshaju@gmail.com>
2022-09-20 07:44:23 +0530

    web browser

---
+++ wb.py
@@ -0,0 +1,43 @@
+import webbrowser as wb
+
+chrome_browser = wb.get("C:\Program Files\Google\Chrome\Application\chrome.exe %s")
+chrome_browser.open_new_tab('hello')
+
+
+
+"""
+sites = [
+"Drawkit",
+"Ouch",
+"Illustratious",
+"Free Web illustrations",
+"Humaaans",
+"Absurd illustrations",
+"Blush",
+"StockSnap",
+"Landing Stock",
+"Nappy",
+"Photo Creator",
+"StockSnap",
+"Reshot",
+"Gratisography",
+"Mckups",
+"Placeit",
+"Smartmockups",
+"Mockuuups",
+"PSD Repo",
+"Mockup",
+"Mockupmark",
+"Mockup's Design",
+"Pixeden",
+"Noun Project",
+"lonicons",
+"Icons",
+"Simple Icons",
+"Dryicons",
+"Mr.lcons",
+"Pixeden",
+"Iconfinder",
+"Caption Icon",
+]
+"""
