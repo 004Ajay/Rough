6bb9a1e866e09d1952bed0929e79c5a961dd2869
Ajay T Shaju <ajaytshaju@gmail.com>
2022-10-09 15:55:32 +0530

    new codes

--- GUI DBMS/dbcnt.py
+++ GUI DBMS/dbcnt.py
@@ -3,15 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="root",
-    database="testdb"
-)
+    passwd="1234",
+    database = 'data'
+    )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM customers")
+mycursor.execute("SELECT * FROM test3")
 
-myresult = mycursor.fetchall()
+res = mycursor.fetchall()
 
-for x in myresult:
+for x in res:
     print(x)
