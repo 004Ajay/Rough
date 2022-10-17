f09b40ff8b565bf73aea3adbd0c4884261253022
Ajay T Shaju <ajaytshaju@gmail.com>
2022-10-17 23:06:42 +0530

    code

--- GUI DBMS/new.py
+++ GUI DBMS/new.py
@@ -5,8 +5,9 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="",
-    database="mydatabase"
+    passwd="1234",
+    database="data",
+    auth_plugin='mysql_native_password'
 )
 
 mycursor = mydb.cursor()
