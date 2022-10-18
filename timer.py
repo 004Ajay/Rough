dd387c8f1e5aed5ba48d75564b112b4cf243ceb0
Ajay T Shaju <ajaytshaju@gmail.com>
2022-10-18 07:46:09 +0530

    timer

--- timer.py
+++ timer.py
@@ -7,10 +7,42 @@ root.title("Countdown Timer")
 
 def countdown():
     t = int(entry_box.get())
-    if t <= 0:
+    while(t):
+        m, s = divmod(t, 60)
+        h, m = divmod(m, 60)
+        #current_time = '{:02d}:{:02d}'.format(mins, secs)
+        #times = '{:02d}:{:02d}'.format(mins, secs)
+        #current_time = strftime(f'{mins}:{secs}')
+        #current_time = time.strftime("%M:%S").format(mins, secs)
+        #current_time = '{:02d}:{:02d}'.format(mins, secs)
+        #current_time = (f'{times[0]}:{times[1]}')
+        current_time = '{:02d}:{:02d}:{:02d}'.format(h, m, s)
+        print(current_time)
+        #clock.config(text=current_time)
+        time_string = current_time
+        clock.config(text = current_time)
+        if time_string != current_time:
+            time_string = current_time
+            clock.config(text=time_string)
+
+        #print(timeformat, end='\r')
+        time.sleep(1)
+        #clock.after(1000, countdown)
+        t -= 1
+    
+        
+        
+"""
+        #global time_string
+        t = int(entry_box.get())
+        if t <= 0:
+        # wait 1 sec and set timer to 00:00
         reset()
         return
-    else:
+        else:
+
+
+
         global time_string
         while(t):
             mins, secs = divmod(t, 60)
@@ -19,17 +51,19 @@ def countdown():
             #current_time = strftime(f'{mins}:{secs}')
             #current_time = time.strftime("%M:%S")
             #current_time = '{:02d}:{:02d}'.format(mins, secs)
-            current_time = (f'{mins}:{secs}')
-            if current_time != time_string:
+            current_time = str(f'{mins}:{secs}')
+            clock.config(text=time_string)
+
+            if time_string != current_time:
                 time_string = current_time
                 clock.config(text=time_string)
             
             #print(timeformat, end='\r')
-            #time.sleep(1)
-            clock.after(1000, countdown)
+            time.sleep(1)
+            #clock.after(1000, countdown)
             t -= 1
             
-            
+"""
 
 def start():
     global running
@@ -44,13 +78,13 @@ def stop():
 def reset():
     global running
     running = False
-    time_string = "0"
+    time_string = "00:00:00"
     clock.config(text=time_string)
 
 def exit():
     root.destroy()
 
-clock = Label(root, font=("times", 50, "bold"))
+clock = Label(root, font=("helvatica", 50, "normal"))
 clock.grid(row=0, column=1)
 
 start_button = ttk.Button(root, text="Start", command=start)
@@ -73,8 +107,8 @@ entry_box.grid(row=1, column=1, padx=5, pady=5)
 
 #set_button = ttk.Button(root, text="Set", command=countdown)
 #set_button.grid(row=1, column=2, padx=5, pady=5)
-
-time_string = "0"
-clock.config(text=time_string)
+#clock = StringVar()
+time_string = "00:00:00"
+clock.config(text = time_string)
 
 root.mainloop()
