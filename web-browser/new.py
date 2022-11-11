24d914c7fa6307fe23ce12a143766b384e56f011
Ajay T Shaju <ajaytshaju@gmail.com>
2022-11-11 12:06:16 +0530

    new file

---
+++ web-browser/new.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+url = 'https://docs.python.org/'
+url2 = 'https://google.com/'
+
+# Open URL in a new tab, if a browser window is already open.
+#webbrowser.open_new_tab(url)
+
+# Open URL in new window, raising the window if possible.
+webbrowser.open_new(url2)
