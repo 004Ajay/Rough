dfd47a3745bf91efacbeedcf546e98c6f13f1072
Ajay T Shaju <ajaytshaju@gmail.com>
2022-11-12 23:05:12 +0530

    cv

--- pencil_sketch.py
+++ pencil_sketch.py
@@ -1,3 +1,10 @@
 import cv2
 
-image = cv2.imread('')
+image = cv2.imread('img.jpg')
+gray_im = cv2.cvtColor(image, cv2.COLOR_BGRA2GRAY)
+inverted = 255-gray_im
+cv2.imshow("img", inverted)
+blur = cv2.GaussianBlur(inverted, (21, 21), 0)
+invertedBlur = 255-blur
+sketch = cv2.divide(gray_im, invertedBlur, scale=256.0)
+cv2.imwrite("sketch_im.jpg", sketch)
