9c0c8a2a952a73df81084aadaa75ebc3badcb801
Ajay T Shaju <ajaytshaju@gmail.com>
2022-11-16 19:39:45 +0530

    changes

--- pencil_sketch.py
+++ pencil_sketch.py
@@ -1,10 +1,10 @@
 import cv2
 
-image = cv2.imread('img.jpg')
+image = cv2.imread('C:/Users/ASUS/Desktop/anj.jpg')
 gray_im = cv2.cvtColor(image, cv2.COLOR_BGRA2GRAY)
 inverted = 255-gray_im
 cv2.imshow("img", inverted)
 blur = cv2.GaussianBlur(inverted, (21, 21), 0)
 invertedBlur = 255-blur
-sketch = cv2.divide(gray_im, invertedBlur, scale=256.0)
+sketch = cv2.divide(gray_im, invertedBlur, scale=240.0)
 cv2.imwrite("sketch_im.jpg", sketch)
